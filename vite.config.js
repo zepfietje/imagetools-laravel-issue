4bd7192158bab5f69803937c782eb3cab67f3405
Zep Fietje <hey@zepfietje.com>
2024-07-13 11:50:35 +0200

    Install vite-imagetools

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+import { imagetools } from 'vite-imagetools';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        imagetools(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
